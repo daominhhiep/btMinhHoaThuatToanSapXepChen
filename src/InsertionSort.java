6f83bd1501af20547594f9348ca4ede0c5a7c748
admin <daominhhiep97@gmail.com>
2020-07-23 09:39:14 +0700

    [Bài tập] Minh hoạ thuật toán sắp xếp chèn

---
+++ src/InsertionSort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void insertionSortByStep(int[] list)  {
+        for (int i = 1; i < list.length; i++){
+            int currentElement = list[i];
+            int k;
+            for(k = i - 1; k >=0 && list[k] > currentElement; k--){
+                System.out.println("Swap " + list[k+1] + " with " + list[k]);
+                list[k+1] = list[k];
+            }
+            list[k+1] = currentElement;
+
+            System.out.print("List after the " + i + " sort: ");
+            for (int j = 0; j < list.length; j++) {
+                System.out.print(list[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+        insertionSortByStep(list);
+
+    }
+}
